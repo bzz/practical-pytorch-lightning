4775e6decc4df08f64715eceeedce8c17a674753
Alexander Bezzubov <bzz@apache.org>
2020-01-23 15:06:27 +0100

    data: add fetching codesearchnet + vocab
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- download_data.sh
+++ download_data.sh
@@ -9,3 +9,13 @@ then
   mkdir -p data
   mv *.txt data
 fi
+
+# fetching CodeSearchNet + pre-built vocabulary
+if ! stat -t data/codesearchnet >/dev/null 2>&1
+then
+  wget 'https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/java.zip'
+  mkdir -p data/codesearchnet
+  unzip java.zip
+  mv java data/codesearchnet
+  wget 'https://gist.github.com/bzz/cfa601b589893b4e709de98acdf0109f/raw/9dbfa3c086a1bf4ae745996242885f149f5e28d6/vocab.programming_function_name.8192.subwords'
+fi
